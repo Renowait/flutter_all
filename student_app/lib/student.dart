1add7788e20221d800b97192502e6a91b58de1bf
Renowait <3651051541147@rmutr.ac.th>
2025-01-20 12:46:40 +0700

    two

---
+++ student_app/lib/student.dart
@@ -0,0 +1,13 @@
+class Student {
+  static List<Map<String,dynamic>> studentItems = [
+    {'id':'3651051541103','name':'นางสาวสาวจิดาภา แก้วสกุล','score':19},
+    {'id':'3651051541108','name':'นางสาวอาทิตยา ผงทอง','score':18},
+    {'id':'3651051541120','name':'นายคมิก บัวทรัพย์','score':19},
+    {'id':'3651051541121','name':'นางสาวณัฐญา อุดมสุข','score':20},
+    {'id':'3651051541121','name':'นางสาววีรวรรณ เผ่าผาง','score':18},
+    {'id':'3651051541147','name':'นายนธีพัฒน์ เถื่อนกวา','score':99},
+  ];
+  static int countItem=studentItems.length;
+  static Function updateStudentItems = () {};
+  static Function addStudentItems = () {};
+}
