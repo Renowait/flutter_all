a06666864aa214d3e9dd79b098dc0d98cc180481
Renowait <3651051541147@rmutr.ac.th>
2025-02-10 08:57:07 +0700

    four

--- api_app/lib/get_todos.dart
+++ api_app/lib/get_todos.dart
@@ -14,13 +14,14 @@ class GetTodos extends StatefulWidget {
 class _GetTodosState extends State<GetTodos> {
   Future<Map<String, dynamic>> getTodo(int id) async {
     var url = "https://jsonplaceholder.typicode.com/todos/$id";
-    var respones = await http.get(Uri.parse(url));
+    var response = await http.get(Uri.parse(url)); 
 
-    return jsonDecode(respones.body) as Map<String, dynamic>;
+    return jsonDecode(response.body) as Map<String, dynamic>;
   }
 
   Future<Map<String, dynamic>>? todo;
   var text = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,34 +29,35 @@ class _GetTodosState extends State<GetTodos> {
         title: const Text('Restful API'),
       ),
       body: Container(
-          alignment: Alignment.topCenter,
-          padding: const EdgeInsets.symmetric(
-            vertical: 50,
-            horizontal: 20,
-          ),
-          child: Column(
-            children: [
-              Text(text),
-              const SizedBox(
-                height: 30,
-              ),
-            ],
-          )),
+        alignment: Alignment.topCenter,
+        padding: const EdgeInsets.symmetric(
+          vertical: 50,
+          horizontal: 20,
+        ),
+        child: Column(
+          children: [
+            Text(text),
+            const SizedBox(height: 30),
+            btnRandom(), 
+          ],
+        ),
+      ),
     );
   }
 
   Widget btnRandom() {
     return TextButton(
-        onPressed: () {
-          var id = Random().nextInt(30) + 1;
-          todo = getTodo(id);
-          todo?.then((value) {
-            setState(() {
-              text = 'Title: ${value['title']}\n';
-              text += 'Complete: ${value['Complere']}\n';
-            });
+      onPressed: () {
+        var id = Random().nextInt(30) + 1;
+        todo = getTodo(id);
+        todo?.then((value) {
+          setState(() {
+            text = 'Title: ${value['title']}\n';
+            text += 'Complete: ${value['completed']}\n'; 
           });
-        },
-        child: const Text('Random Todo'));
+        });
+      },
+      child: const Text('Random Todo'),
+    );
   }
 }
